1a42416b58a8db2cf3b7f52e81d60a3dc79d88fc
Trung <trung3566@gamil.com>
2020-11-17 14:15:20 +0700

    Add new class

---
+++ MainGame.java
@@ -0,0 +1,13 @@
+package shootfeedFish;
+
+public class MainGame {
+	public static void main(String[] args) {
+		
+	   OpenningWindow ow = new OpenningWindow();
+		//MenuWindow m = new MenuWindow();
+		//PlayWindow pw = new PlayWindow(); 
+		//Level1design l1 = new Level1design(); 
+	}
+
+}
+	
