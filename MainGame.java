ce662b56e9d26dc88606c20d9d49898749e1aa81
Lý Minh Trung <trung3566@gmail.com>
2020-11-23 13:43:24 +0700

    Add classes

--- MainGame.java
+++ MainGame.java
@@ -3,10 +3,11 @@ package shootfeedFish;
 public class MainGame {
 	public static void main(String[] args) {
 		
-	   OpenningWindow ow = new OpenningWindow();
+	  // OpenningWindow ow = new OpenningWindow();
 		//MenuWindow m = new MenuWindow();
-		//PlayWindow pw = new PlayWindow(); 
+		PlayWindow pw = new PlayWindow(); 
 		//Level1design l1 = new Level1design(); 
+		 //Level2design l2 = new Level2design(); 
 	}
 
 }
